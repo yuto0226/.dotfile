489ada752f4e2cf298c9a2e702ade68431deda0d
yuto0226 <wayne71112@gmail.com>
2024-10-05 13:34:37 +0800

    Update LazyVim config

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -48,6 +48,6 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
-      },
     },
+  },
 }
---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    event = "VeryLazy",
+    ops = {
+      section_separators = { left = '', right = '' },
+      component_separators = { left = '', right = '' }
+    },
+  }
+}
