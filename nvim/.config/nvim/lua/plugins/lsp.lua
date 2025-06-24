a73892a4f56fba08b8d472a6b12d8dafceaf68a0
yuto0226 <wayne71112@gmail.com>
2025-06-24 20:39:41 +0800

    Change icons for mason

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -7,7 +7,15 @@ return {
     -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
     {
       'mason-org/mason.nvim',
-      opts = {},
+      opts = {
+        ui = {
+          icons = {
+            package_installed = "",
+            package_pending = "",
+            package_uninstalled = "",
+          },
+        }
+      },
     },
     'mason-org/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
