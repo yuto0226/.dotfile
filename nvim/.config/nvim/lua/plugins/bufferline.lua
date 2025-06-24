856a8b7a26ffcfa1b2f46fa76993849118e3a4d0
yuto0226 <wayne71112@gmail.com>
2025-06-24 20:37:19 +0800

    Change buffer close icon

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -11,8 +11,8 @@ return {
         themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
         numbers = 'none', -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
         close_command = 'Bdelete! %d', -- can be a string | function, see "Mouse actions"
-        buffer_close_icon = '',
-        close_icon = '',
+        buffer_close_icon = '',
+        close_icon = '',
         path_components = 1, -- Show only the file name without the directory
         modified_icon = '●',
         left_trunc_marker = '',
