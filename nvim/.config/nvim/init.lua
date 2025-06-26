de3fdfe967dfbd052df58860962bafa2d388ad89
yuto0226 <wayne71112@gmail.com>
2025-06-26 15:40:38 +0800

    Add markdown support

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -9,7 +9,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out, 'WarningMsg' },
+      { out,                            'WarningMsg' },
       { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
@@ -28,20 +28,21 @@ vim.g.maplocalleader = '\\'
 require('lazy').setup {
   spec = {
     -- add your plugins here
-    require 'plugins.colortheme', -- Catppuccin color themes
-    require 'plugins.neotree', -- File explorer
-    require 'plugins.bufferline', -- Tab management
-    require 'plugins.lualine', -- Status line
-    require 'plugins.treesitter', -- Syntax highlighting and code navigation
-    require 'plugins.telescope', -- Fuzzy finder
-    require 'plugins.lsp', -- Language Server Protocol support
-    require 'plugins.cmp', -- Autocompletion
-    require 'plugins.none-ls', -- Linting and formatting
-    require 'plugins.gitsigns', -- Git integration
-    require 'plugins.alpha', -- Startup dashboard
+    require 'plugins.colortheme',       -- Catppuccin color themes
+    require 'plugins.neotree',          -- File explorer
+    require 'plugins.bufferline',       -- Tab management
+    require 'plugins.lualine',          -- Status line
+    require 'plugins.treesitter',       -- Syntax highlighting and code navigation
+    require 'plugins.telescope',        -- Fuzzy finder
+    require 'plugins.lsp',              -- Language Server Protocol support
+    require 'plugins.cmp',              -- Autocompletion
+    require 'plugins.none-ls',          -- Linting and formatting
+    require 'plugins.gitsigns',         -- Git integration
+    require 'plugins.alpha',            -- Startup dashboard
     require 'plugins.indent-blankline', -- Indentation guides
-    require 'plugins.misc', -- Miscellaneous plugins
-    require 'plugins.rust', -- rust
+    require 'plugins.misc',             -- Miscellaneous plugins
+    require 'plugins.rust',             -- rust
+    require 'plugins.markdown',         -- markdown
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
---
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
+    '3rd/image.nvim',
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {
+      processor = 'magick_cli',
+    },
+  },
+}
